ccef9b9a1e2934b9b25026669ec5dacb4c6df7fc
Laurence Yamamoto <i.yamamoto@hotmail.com>
2024-10-17 18:46:11 +0900

    Update AudioStream.java

--- app-server/src/main/java/jp/co/gafs/transcriber/sv/model/AudioStream.java
+++ app-server/src/main/java/jp/co/gafs/transcriber/sv/model/AudioStream.java
@@ -1,5 +1,15 @@
 package jp.co.gafs.transcriber.sv.model;
 
-public class AudioStream {
+import java.io.File;
+import java.nio.file.Paths;
 
+import jp.co.gafs.transcriber.sv.config.Config;
+
+public class AudioStream extends StreamWriter<byte[]> {
+    public AudioStream(String sessionId, int requestId, String extension) {
+        super(
+            Paths.get(Config.getAudioFolderPath() + File.separator + sessionId + "-" + requestId + Config.getAudioFileSuffix() + extension),
+            (byte[] array) -> array
+        );
+    }
 }
