b8f2971b700456e6a4ef461c7850d668e1088862
Laurence Yamamoto <i.yamamoto@hotmail.com>
2024-10-17 18:46:05 +0900

    Update ApplicationException.java

--- app-server/src/main/java/jp/co/gafs/transcriber/sv/exception/ApplicationException.java
+++ app-server/src/main/java/jp/co/gafs/transcriber/sv/exception/ApplicationException.java
@@ -1,5 +1,33 @@
 package jp.co.gafs.transcriber.sv.exception;
 
-public class ApplicationException {
-    
+public class ApplicationException extends RuntimeException {
+
+    private int httpStatusCode;
+    private String internalMessage;
+
+    public ApplicationException(String message) {
+        this(message, null, 500, message);
+    }
+
+    public ApplicationException(String message, Throwable cause) {
+        this(message, cause, 500, message);
+    }
+
+    public ApplicationException(String message, Throwable cause, int httpStatusCode) {
+        this(message, cause, httpStatusCode, message);
+    }
+
+    public ApplicationException(String message, Throwable cause, int httpStatusCode, String internalMessage) {
+        super(message, cause);
+        this.httpStatusCode = httpStatusCode;
+        this.internalMessage = internalMessage;
+    }
+
+    public int getHttpStatusCode() {
+        return httpStatusCode;
+    }
+
+    public String getInternalMessage() {
+        return internalMessage;
+    }
 }
